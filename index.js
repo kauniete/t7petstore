bb5c5341732de61349ff9add77ae2e3afc34ba12
kauniete <38038319+kauniete@users.noreply.github.com>
2018-10-29 19:14:42 +0100

    populate template

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ function showPets(petList){
     petList.forEach(showSinglePet)
 }
 function showSinglePet(pet){
-    console.log(pet.title.rendered)
+    console.log(pet)
     //make a copy of the template
     const copy = template.cloneNode(true);
     copy.querySelector("h1").textContent=pet.title.rendered;
+    copy.querySelector(".type").textContent=pet.acf.types_of_pets;
+    copy.querySelector(".color").textContent=pet.acf.color;
+    copy.querySelector(".age").textContent=pet.acf.age + " months";
+    copy.querySelector(".weight").textContent=pet.acf.weight + " kg.";
+    copy.querySelector(".lifetime").textContent=pet.acf.lifetime + " years";
+    copy.querySelector(".origin").textContent=pet.acf.country_of_origin;
+    copy.querySelector(".price").textContent=pet.acf.price + " EUR";
+    copy.querySelector(".body").innerHTML=pet.content.rendered;
+
     document.querySelector("main").appendChild(copy)
 }
 getPets();
